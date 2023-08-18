16c54cecb11698f0ce420ece790be22a2d7d47d8
ivanpudin <ivanpudin9@gmail.com>
2023-08-18 11:00:16 +0300

    Add modules and clean the code.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div className="justify-center items-center">
+      <header className="text-xl">
+        Hello there!
+      </header>
+    </div>
+  );
+}
+
+export default App;
